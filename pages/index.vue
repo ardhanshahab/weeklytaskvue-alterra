15362053bc397b4a076a0a29eeed274469808ccb
ardhanshahab <ardhanshahab@gmail.com>
2022-07-22 19:51:05 +0700

    feat : first

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="landing"> 
+  <b-button pill variant="primary">Weekly Task 1</b-button>
+  <b-button pill variant="primary">Weekly Task 2</b-button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style scoped>
+.landing {
+  display: flex;
+  align-content: center;
+}
+</style>
